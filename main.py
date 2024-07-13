5d955e3b20a9f050d9ef1be0e8319c0f9b35ab4c
MikhailTo <toshkin.mikhail@yandex.ru>
2024-07-13 06:45:49 +0300

    Initial_commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import random
+import telebot
+
+bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
+
+
+# ---------------- dialog params ----------------
+dialog = {
+    'hello': {
+        'in': ['/hello', 'привет', 'hello', 'hi', 'privet', 'hey'],
+        'out': ['Приветствую', 'Здравствуйте', 'Привет!']
+    },
+    'how r u': {
+        'in': ['/howru', 'как дела', 'как ты', 'how are you', 'дела', 'how is it going'],
+        'out': ['Хорошо', 'Отлично', 'Good. And how are u?']
+    },
+    'name': {
+        'in': ['/name', 'зовут', 'name', 'имя'],
+        'out': [
+            'Я telegram-template-bot',
+            'Я бот шаблон, но ты можешь звать меня в свой проект',
+            'Это секрет. Используй команду /help, чтобы узнать'
+        ]
+    }
+}
+
+
+# --------------------- bot ---------------------
+@bot.message_handler(commands=['help', 'start'])
+def say_welcome(message):
+    user_first_name = str(message.chat.first_name) 
+    #bot.reply_to(message, f"Привет! {user_first_name} \n Добро пожаловать!")
+    bot.send_message(message.chat.id,
+                     'Добро пожаловать, ', user_first_name, '!\n',
+                     parse_mode='markdown')
+
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    for t, resp in dialog.items():
+        if sum([e in message.text.lower() for e in resp['in']]):
+            bot.send_message(message.chat.id, random.choice(resp['out']))
+            return
+
+    bot.send_message(message.chat.id, 'Проблемы?. Воспользуйтесь /help.')
+
+
+# ---------------- local testing ----------------
+if __name__ == '__main__':
+    bot.infinity_polling()

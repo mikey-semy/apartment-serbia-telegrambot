57d4d3c35cdab05a6800b2dd2b35239430fe2e9a
Mikhail Toshkin <toshkin.mikhail@yandex.ru>
2024-07-22 05:39:54 +0000

    add forzida scraper

--- scraper.py
+++ scraper.py
@@ -65,7 +65,7 @@ class NekretnineScraper(WebScraper):
 
     def has_next_page(self, soup) -> bool:
         # Проверяем, есть ли следующая страница
-        next_page_link = soup.find('a', class_=self.data["NEXT_BUTTON"])  # Признак окончания узнать, поправить.
+        next_page_link = soup.find(self.data["NEXT_BUTTON_TAG"], class_=self.data["NEXT_BUTTON_CLASS"])  # Признак окончания узнать, поправить.
         return next_page_link is not None
 
     def scrape_page(self, soup) -> list:
@@ -73,7 +73,7 @@ class NekretnineScraper(WebScraper):
         # Скрейпим страницу и возвращаем список предложений по отдельности
         offers = []
         pattern = r"^\s+|\n|\r|\s+$"
-        offer_elements = soup.findAll('div', class_=self.data["OFFER_CLASS"])
+        offer_elements = soup.findAll(self.data["OFFER_TAG"], class_=self.data["OFFER_CLASS"])
 
         for offer_element in offer_elements:
             self.scrape_pause()
@@ -100,6 +100,63 @@ class NekretnineScraper(WebScraper):
             }) if title else ''  # Исключаем результат без заголовка.
         return offers
 
+class FourzidaScraper(WebScraper):
+
+    def __init__(self, url, page_number=1):
+        # Инициализируем базовый скрейпер с URL и номером страницы
+        super().__init__(url, {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+        })
+        self.name = "4zida"
+        self.url = url
+        self.data = self.scraper_data[self.name]
+        # Конструируем URL для следующей страницы
+        if page_number > 1:
+            self.url += self.data["NEXT_PAGE"].format(page_number=page_number)
+
+    def scrape(self) -> list:
+        # Скрейпим страницу и возвращаем список предложений на странице
+        soup = self.get_page(self.url)
+        return self.scrape_page(soup)
+
+    def has_next_page(self, soup) -> bool:
+        # Проверяем, есть ли следующая страница
+        next_page_link = soup.find(self.data["NEXT_BUTTON_TAG"], class_=self.data["NEXT_BUTTON_CLASS"])  # Признак окончания узнать, поправить.
+        return next_page_link is not None
+
+    def scrape_page(self, soup) -> list:
+        # Скрейпинг nekretnine.rs
+        # Скрейпим страницу и возвращаем список предложений по отдельности
+        offers = []
+        pattern = r"^\s+|\n|\r|\s+$"
+        offer_elements = soup.findAll(self.data["OFFER_TAG"], class_=self.data["OFFER_CLASS"])
+
+        for offer_element in offer_elements:
+            self.scrape_pause()
+            # Извлекаем данные из элемента предложения
+            title = offer_element.find(self.data["TITLE_TAG"], class_=self.data["TITLE_CLASS"])
+            location = offer_element.find(self.data["LOCATION_TAG"], class_=self.data["LOCATION_CLASS"])
+            rooms = offer_element.find(self.data["ROOMS_TAG"], class_=self.data["ROOMS_CLASS"])
+            # date_create = offer_element.find(self.data["DATE_CREATE_TAG"], class_=self.data["DATE_CREATE_CLASS"])
+            price = offer_element.find(self.data["PRICE_TAG"], class_=self.data["PRICE_CLASS"])
+            area = offer_element.find(self.data["AREA_TAG"], class_=self.data["AREA_CLASS"])
+            floor = offer_element.find(self.data["FLOOR_TAG"], class_=self.data["FLOOR_CLASS"])
+            url_offer = offer_element.find(self.data["LINK_HREF_OFFER_TAG"], class_=self.data["LINK_HREF_OFFER_CLASS"])
+            url_image = offer_element.find(self.data["IMG_SRC_OFFER_TAG"], class_=self.data["IMG_SRC_OFFER_CLASS"])
+
+            # Очищаем данные и добавляем их в список предложений
+            offers.append({
+                'title': re.sub(pattern, '', str(title.text)),
+                'location': re.sub(pattern, '', str(location.text)),
+                'rooms': re.sub(pattern, '', str(rooms.text)).split(' • ')[1],  # Нужно сделать проверку
+                # 'date_create': re.sub(pattern, '', str(date_create.text)).split(' | ')[0],
+                'price': price.find('span').text.strip(),
+                'area': re.sub(pattern, '', str(area.text)).split(' • ')[0],
+                'floor': re.sub(pattern, '', str(floor.text)).split(' • ')[2],
+                'url_offer': self.data["BASE_URL"] + url_offer.get('href'),
+                'url_image': url_image.find('img').get('src')
+            }) if title else ''  # Исключаем результат без заголовка.
+        return offers
 # To check separately:
 
 
@@ -124,6 +181,7 @@ class NekretnineScraper(WebScraper):
 
 #         return all_offers
 
-# url = 'https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/prodaja/grad/beograd/lista/po-stranici/10/'
+# urlNekretnine = 'https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/prodaja/grad/beograd/lista/po-stranici/10/'
+# urlFourzida = 'https://www.4zida.rs/prodaja-stanova/beograd/garsonjera/vlasnik/do-100000-evra?struktura=jednosoban&struktura=jednoiposoban&struktura=dvosoban&struktura=dvoiposoban&struktura=trosoban&vece_od=10m2&manje_od=60m2&skuplje_od=1000eur'
 # offers = get_data(url, 2)
 # print(offers)

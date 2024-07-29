31bc19bb33be466bbb378e6ae128e253b59d9170
Mikhail Toshkin <34812500+MikhailTo@users.noreply.github.com>
2024-07-29 05:45:52 +0000

    folders old and new for files "no for project"

--- utils/jsonloader.py
+++ utils/jsonloader.py
@@ -1,16 +1,15 @@
 import os
 import json
 
-ENCODING = 'UTF-8'
-
 class JsonLoader:
+    ENCODING = 'UTF-8'
     def __init__(self, file_name):
         self.__db_path = os.getcwd()
         self.__json_file = os.path.join(self.__db_path, file_name)
 
     def load_json(self):
         try:
-            with open(self.__json_file, 'r', encoding=ENCODING) as f:
+            with open(self.__json_file, 'r', encoding=self.ENCODING) as f:
                 return json.load(f)
         except (json.JSONDecodeError, FileNotFoundError) as e:
             print(f"Error loading JSON file: {e}")
